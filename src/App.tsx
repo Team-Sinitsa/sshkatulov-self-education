4323fcf79a18576595f1268b3bc9c67067d24c90
s.shkatulov <s.shkatulov@gmail.com>
2021-11-19 18:24:00 +0100

    added i18n

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
 import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import { useTranslation, Trans } from 'react-i18next';
 
 import logo from './logo.svg';
 import './App.css';
 
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
+const lngs: any = {
+  en: { nativeName: 'English' },
+  po: { nativeName: 'Polish' }
+};
+
 function App() {
   const theme = useTheme();
   const colorMode = React.useContext(ColorModeContext);
+  const { t, i18n } = useTranslation();
   return (
     <Box
       sx={{
@@ -25,18 +32,27 @@ function App() {
     >
       <div className="App">
         <div className="top-bar">
-          <IconButton className="theme-switcher" sx={{ ml: 0 }} onClick={colorMode.toggleColorMode} color="inherit">
+          <div>
+            {Object.keys(lngs).map((lng) => (
+              <button className="control-switcher" key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+                {lngs[lng].nativeName}
+              </button>
+            ))}
+          </div>
+          <IconButton className="control-switcher" sx={{ ml: 0 }} onClick={colorMode.toggleColorMode} color="inherit">
             {theme.palette.mode}
           </IconButton>
         </div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit
-            {' '}
-            <code>src/App.tsx</code>
-            {' '}
-            and save to reload.
+            <Trans i18nKey="description.part1">
+              Edit
+              {' '}
+              <code>src/App.tsx</code>
+              {' '}
+              and save to reload.
+            </Trans>
           </p>
           <a
             className="App-link"
@@ -44,7 +60,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            {t('description.part2')}
           </a>
         </header>
       </div>
